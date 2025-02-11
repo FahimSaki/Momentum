a34d6069f387206181e975f7803aa704d4fa181b
Fahimuzzaman Saki <fahimuzzamaninferno@gmail.com>
2025-02-11 22:53:51 +0600

    Major drawer changes and module import

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/components/drawer_tile.dart';
+import 'package:habit_tracker/pages/settings_page.dart';
 import 'package:habit_tracker/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -8,14 +10,61 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isLightMode = !themeProvider.isDarkMode;
+
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      child: Center(
-        child: CupertinoSwitch(
-          value: Provider.of<ThemeProvider>(context).isDarkMode,
-          onChanged: (value) =>
-              Provider.of<ThemeProvider>(context, listen: false).toggleTheme(),
-        ),
+      child: Column(
+        children: [
+          // notes logo
+          Theme(
+            data: Theme.of(context).copyWith(
+              dividerTheme: const DividerThemeData(color: Colors.transparent),
+            ),
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context)
+                    .colorScheme
+                    .surface, // use your drawer background color
+              ),
+              child: Center(
+                child: Image.asset(
+                  isLightMode
+                      ? 'assets/images/notes_logo_lightmode.png'
+                      : 'assets/images/notes_logo_darkmode.png',
+                  width: 170,
+                  height: 170,
+                ),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 25.0),
+          // const Spacer(),
+
+          // notes tile
+          DrawerTile(
+            title: 'Home',
+            leading: const Icon(Icons.home),
+            onTap: () => Navigator.pop(context),
+          ),
+
+          // settings tile
+          DrawerTile(
+            title: 'Settings',
+            leading: const Icon(Icons.settings),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsPage(),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
