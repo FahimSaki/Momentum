8699d03f2610500ee1d8279aaf39d3b7dd8f0460
Fahimuzzaman Saki <fahimuzzamaninferno@gmail.com>
2025-02-20 02:45:32 +0600

    env fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,18 @@ import 'package:habit_tracker/pages/home_page.dart';
 import 'package:habit_tracker/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
   // Initialize Supabase
   await Supabase.initialize(
-    ***REMOVED***
-    ***REMOVED***
-        ***REMOVED***
+    url: dotenv.env['SUPABASE_URL']!,
+    ***REMOVED*** dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   // initialize habit database
