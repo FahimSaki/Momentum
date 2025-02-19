e3f706be6ea4ddefc6c2960b31cd2480511b9356
Fahimuzzaman Saki <fahimuzzamaninferno@gmail.com>
2025-02-20 02:38:07 +0600

    supabase added

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:habit_tracker/database/habit_database.dart';
 import 'package:habit_tracker/pages/home_page.dart';
 import 'package:habit_tracker/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: 'https://wzuvknzfarozjikijutt.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6dXZrbnpmYXJvemppa2lqdXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5OTAzODUsImV4cCI6MjA1NTU2NjM4NX0.FN-zFRbuHF7i9lMhYb7ulIigAf7ZzvlJoIsUH81aUFo',
+  );
+
   // initialize habit database
   await HabitDatabase.init();
   await HabitDatabase().saveFirstLaunchDate();
