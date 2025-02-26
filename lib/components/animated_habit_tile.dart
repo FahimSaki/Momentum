4b31ad4e10621a3509457dfe853f3ced7f99f4ef
Fahimuzzaman Saki <fahimuzzamaninferno@gmail.com>
2025-02-27 01:27:45 +0600

    drop down animation

---
+++ lib/components/animated_habit_tile.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:habit_tracker/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class AnimatedHabitTile extends StatefulWidget {
+  final String text;
+  final bool isCompleted;
+  final Function(bool?)? onChanged;
+  final void Function(BuildContext)? editHabit;
+  final void Function(BuildContext)? deleteHabit;
+
+  const AnimatedHabitTile({
+    super.key,
+    required this.isCompleted,
+    required this.text,
+    required this.onChanged,
+    required this.editHabit,
+    required this.deleteHabit,
+  });
+
+  @override
+  State<AnimatedHabitTile> createState() => _AnimatedHabitTileState();
+}
+
+class _AnimatedHabitTileState extends State<AnimatedHabitTile>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0, 1.5),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _fadeAnimation = Tween<double>(
+      begin: 1.0,
+      end: 0.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleCompletion() async {
+    if (!widget.isCompleted) {
+      await _controller.forward();
+      widget.onChanged?.call(true);
+    } else {
+      widget.onChanged?.call(false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isLightMode = !themeProvider.isDarkMode;
+
+    return SlideTransition(
+      position: _slideAnimation,
+      child: FadeTransition(
+        opacity: _fadeAnimation,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+          child: Slidable(
+            endActionPane: ActionPane(
+              motion: const StretchMotion(),
+              children: [
+                CustomSlidableAction(
+                  onPressed: widget.editHabit,
+                  backgroundColor:
+                      isLightMode ? Colors.grey.shade600 : Colors.grey.shade800,
+                  borderRadius: BorderRadius.circular(8),
+                  child: const FaIcon(FontAwesomeIcons.penToSquare),
+                ),
+                SlidableAction(
+                  onPressed: widget.deleteHabit,
+                  backgroundColor: Colors.red,
+                  icon: Icons.delete,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ],
+            ),
+            child: GestureDetector(
+              onTap: _handleCompletion,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: widget.isCompleted
+                      ? (isLightMode ? Colors.green : Colors.teal)
+                      : Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: const EdgeInsets.all(12),
+                child: ListTile(
+                  title: Text(
+                    widget.text,
+                    style: TextStyle(
+                      color: widget.isCompleted
+                          ? Colors.white
+                          : Theme.of(context).colorScheme.inversePrimary,
+                    ),
+                  ),
+                  leading: Checkbox(
+                    activeColor: isLightMode ? Colors.green : Colors.teal,
+                    value: widget.isCompleted,
+                    onChanged: (value) => _handleCompletion(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/components/habit_list.dart
+++ lib/components/habit_list.dart
@@ -5,14 +5,14 @@
  */
 
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/components/habit_tile.dart';
+import 'package:habit_tracker/components/animated_habit_tile.dart';
 import 'package:habit_tracker/components/completed_habits.dart';
 import 'package:habit_tracker/database/habit_database.dart';
 import 'package:habit_tracker/models/habit.dart';
 import 'package:habit_tracker/util/habit_util.dart';
 import 'package:provider/provider.dart';
 
-class HabitListComponent extends StatelessWidget {
+class HabitListComponent extends StatefulWidget {
   final bool showCompletedHabits;
   final void Function(bool?, Habit) checkHabitOnOff;
   final void Function(BuildContext, Habit) editHabitBox;
@@ -27,11 +27,17 @@ class HabitListComponent extends StatelessWidget {
   });
 
   @override
+  State<HabitListComponent> createState() => _HabitListComponentState();
+}
+
+class _HabitListComponentState extends State<HabitListComponent> {
+  final Map<int, bool> _removedHabits = {};
+
+  @override
   Widget build(BuildContext context) {
     final habitDatabase = context.watch<HabitDatabase>();
     List<Habit> currentHabits = habitDatabase.currentHabits;
 
-    // * Separate completed and uncompleted habits
     List<Habit> completedHabits = currentHabits
         .where((habit) => isHabitCompletedToday(habit.completedDays))
         .toList();
@@ -41,7 +47,6 @@ class HabitListComponent extends StatelessWidget {
 
     return Column(
       children: [
-        // * Uncompleted habits
         if (uncompletedHabits.isEmpty && completedHabits.isEmpty) ...[
           const Padding(
             padding: EdgeInsets.only(top: 80, right: 16, left: 16),
@@ -60,28 +65,45 @@ class HabitListComponent extends StatelessWidget {
             physics: const NeverScrollableScrollPhysics(),
             itemBuilder: (context, index) {
               final habit = uncompletedHabits[index];
-              return HabitTile(
-                isCompleted: false,
-                text: habit.name,
-                onChanged: (p0) => checkHabitOnOff(p0, habit),
-                editHabit: (context) => editHabitBox(context, habit),
-                deleteHabit: (context) => deleteHabitBox(context, habit),
+              return AnimatedSize(
+                duration: const Duration(milliseconds: 300),
+                child: _removedHabits[habit.id] == true
+                    ? const SizedBox.shrink()
+                    : AnimatedHabitTile(
+                        key: ValueKey(habit.id),
+                        isCompleted: false,
+                        text: habit.name,
+                        onChanged: (value) {
+                          if (value == true) {
+                            setState(() {
+                              _removedHabits[habit.id] = true;
+                            });
+                            Future.delayed(const Duration(milliseconds: 300),
+                                () {
+                              widget.checkHabitOnOff(value, habit);
+                            });
+                          } else {
+                            widget.checkHabitOnOff(value, habit);
+                          }
+                        },
+                        editHabit: (context) =>
+                            widget.editHabitBox(context, habit),
+                        deleteHabit: (context) =>
+                            widget.deleteHabitBox(context, habit),
+                      ),
               );
             },
           ),
         ],
-
-        // * Completed habits dropdown
         const SizedBox(height: 10),
         CompletedHabits(
           completedHabits: completedHabits,
-          showCompletedHabits: showCompletedHabits,
-          onChanged: (habit) => (p0) => checkHabitOnOff(p0, habit),
-          editHabit: (context, habit) => editHabitBox(context, habit),
-          deleteHabit: (context, habit) => deleteHabitBox(context, habit),
+          showCompletedHabits: widget.showCompletedHabits,
+          onChanged: (habit) => (p0) => widget.checkHabitOnOff(p0, habit),
+          editHabit: widget.editHabitBox,
+          deleteHabit: widget.deleteHabitBox,
           onExpansionChanged: (expanded) {
-            // Update the state in the parent widget
-            (context as Element).markNeedsBuild();
+            setState(() {});
           },
         ),
       ],
