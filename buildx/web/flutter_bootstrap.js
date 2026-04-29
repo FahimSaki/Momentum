bfba30e13f03319479a26c235cb2a996f2c18c79
FahimSaki <fahimuzzamaninferno@gmail.com>
2026-04-30 01:22:40 +0600

    HWidget v 1.0.0

--- buildx/web/flutter_bootstrap.js
+++ buildx/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "956340385" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "240508851" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
