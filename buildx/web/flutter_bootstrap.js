c1057cc2bb55edfa59d2b206384ef564f785d36c
FahimSaki <fahimuzzamaninferno@gmail.com>
2026-05-21 22:44:14 +0600

    Task bug fix DND

--- buildx/web/flutter_bootstrap.js
+++ buildx/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4143384068" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2479985058" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
